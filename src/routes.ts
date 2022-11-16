b291f1c9b3b02c743c08da7f0744a3442e9e9806
Felipe Moreno <rfelipemoreno@hotmail.com.br>
2022-11-16 17:36:04 -0300

    updating user get endpoint
    updating user get endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ const routes = Router()
 routes.post('/novousuario', new UsersController().create)
 
 routes.get('/listarusuarios', new UsersController().list)
+routes.get('/listarusuario/:id', new UsersController().listOne)
 
 routes.put('/atualizarusuario/:id', new UsersController().update)
 
