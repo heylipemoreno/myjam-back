294af50a924ef40dd7820c190f80bd79b5e60285
EuLuiiz <victorluiz011@gmail.com>
2022-11-18 17:14:06 -0300

    Adicionando middleware global para erros

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import express from 'express'
 import cors from 'cors'
 import { AppDataSource } from './data-source'
 import routes from './routes'
+import errorsMiddleware from './middlewares/errorsMiddleware'
 
 AppDataSource.initialize().then(() => {
     const app = express()
 
     app.use(express.json())
-
     app.use(cors())
-
     app.use(routes)
+    app.use(errorsMiddleware)
 
     app.get('/', (request: express.Request, response: express.Response) => {
         return response.status(200).send('API funcionando corretamente.')
