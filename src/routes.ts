afaa989c5b6a962f95b38a4cd04c54d89930a817
EuLuiiz <victorluiz011@gmail.com>
2022-11-18 14:30:39 -0300

    Configurando Validation, corrigindo detalhes e testando rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import validationMiddleware from './middlewares/validationMiddleware';
 import { ChordsController } from './controllers/ChordsController';
 import { GenresController } from './controllers/GenresController';
 import { UsersController } from './controllers/UsersController';
@@ -10,22 +11,25 @@ import { SongsController } from './controllers/SongsController';
 const routes = Router()
 
 //POST
-routes.post('/users', new UsersController().create)
-routes.post('/genres', new GenresController().create)
-routes.post('/chords', new ChordsController().create)
-routes.post('/questions', new QuestionsController().create)
-routes.post('/lessons', new LessonsController().create)
-routes.post('/classes', new ClassesController().create)
-routes.post('/songs', new SongsController().create)
+routes.post('/users', validationMiddleware.users, new UsersController().create)
+routes.post('/genres', validationMiddleware.genres, new GenresController().create)
+routes.post('/chords', validationMiddleware.chords, new ChordsController().create)
+routes.post('/questions', validationMiddleware.questions, new QuestionsController().create)
+routes.post('/lessons', validationMiddleware.lessons, new LessonsController().create)
+routes.post('/classes', validationMiddleware.classes, new ClassesController().create)
+routes.post('/songs', validationMiddleware.songs, new SongsController().create)
 
 //GET ALL
 routes.get('/users', new UsersController().list)
 routes.get('/genres', new GenresController().list)
 routes.get('/chords', new ChordsController().list)
-routes.get('/questions',new QuestionsController().list)
+routes.get('/questions', new QuestionsController().list)
 routes.get('/lessons', new LessonsController().list)
 routes.get('/classes', new ClassesController().list)
 routes.get('/songs', new SongsController().list)
+
+//LOGIN
+
 
 //GET ID
 routes.get('/users/:id', new UsersController().listOne)
@@ -37,13 +41,13 @@ routes.get('/classes/:id', new ClassesController().listOne)
 routes.get('/songs/:id', new SongsController().listOne)
 
 //UPDATE
-routes.put('/users/:id', new UsersController().update)
-routes.put('/genres/:id', new GenresController().update)
-routes.put('/chords/:id', new ChordsController().update)
-routes.put('/questions/:id', new QuestionsController().update)
-routes.put('/lessons/:id', new LessonsController().update)
-routes.put('/classes/:id', new ClassesController().update)
-routes.put('/songs/:id', new SongsController().update)
+routes.put('/users/:id', validationMiddleware.users, new UsersController().update)
+routes.put('/genres/:id', validationMiddleware.genres, new GenresController().update)
+routes.put('/chords/:id', validationMiddleware.chords, new ChordsController().update)
+routes.put('/questions/:id', validationMiddleware.questions, new QuestionsController().update)
+routes.put('/lessons/:id', validationMiddleware.lessons, new LessonsController().update)
+routes.put('/classes/:id', validationMiddleware.classes, new ClassesController().update)
+routes.put('/songs/:id', validationMiddleware.songs, new SongsController().update)
 
 //DELETE
 routes.delete('/users/:id', new UsersController().delete)
@@ -53,5 +57,9 @@ routes.delete('/questions/:id', new QuestionsController().delete)
 routes.delete('/lessons/:id', new LessonsController().delete)
 routes.delete('/classes/:id', new ClassesController().delete)
 routes.delete('/songs/:id', new SongsController().delete)
+
+
+//ATUALIZAÇÕES
+
 
 export default routes
