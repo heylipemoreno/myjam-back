f23bfefea2419fe3e28fdc80acbd80ee8d324bac
EuLuiiz <victorluiz011@gmail.com>
2022-11-19 13:40:01 -0300

    Implementação do Login (Token JWT)

---
+++ src/controllers/LoginController.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express";
+import { Users } from "../entities/Users";
+import { UsersRepository } from "../repositories/UsersRepository";
+import bcrypt from 'bcryptjs';
+import * as jtw from 'jsonwebtoken';
+import { secretKey } from "../config/secret/secret";
+import constants from "../config/constants/constants";
+
+export class LoginController {
+    async login(req: Request, res: Response) {
+        const { email, password } = req.body
+        if (!email && !password) {
+            res.status(400).send(constants.ERROR.MESSAGE.VALIDATION);
+        }
+        try {
+            const user = await UsersRepository.findOneOrFail({
+                where: { email }
+            })
+
+            if (!user) {
+                res.status(404).send('Credenciais não encontrada.')
+            }
+
+            if (!bcrypt.compareSync(password, user.password)) {
+                res.status(401).send('Senha incorreta.')
+            }
+
+            const token = jtw.sign({
+                id: user.id,
+                userName: user.userName,
+                qtdSongs: user.qtdSongs,
+                qtdChords: user.qtdChords
+            }, secretKey, {
+                expiresIn: '1 day'
+            })
+
+            res.status(200).send({
+                User: user,
+                Token: token
+            })
+        } catch (error) {
+            console.log(error)
+            res.status(500).send('error error error')
+        }
+    }
+}
