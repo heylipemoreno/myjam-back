a284da882815580ceea24901d86e51c00d014b56
EuLuiiz <victorluiz011@gmail.com>
2022-11-27 10:41:20 -0300

    Teste de seed fake

--- src/modules/instrument/controllers/instrumentController.ts
+++ src/modules/instrument/controllers/instrumentController.ts
@@ -6,52 +6,52 @@ import listIDInstrumentUseCase from '../usecases/listIDInstrumentUseCase';
 import listInstrumentUseCase from '../usecases/listInstrumentUseCase';
 import updateInstrumentUseCase from '../usecases/updateInstrumentUseCase';
 
-export class InstrumentController{
-    async create(request:express.Request,response:express.Response){
-        try{
-            const instrument=createInstrumentUseCase.execute(request.body);
+export class InstrumentController {
+    async create(request: express.Request, response: express.Response) {
+        try {
+            const instrument = createInstrumentUseCase.execute(request.body);
             response.status(201).send(instrument);
-        } catch(error){
+        } catch (error) {
             console.log(error);
             response.status(500).send(constants.CRUD.ERROR);
         }
     }
 
-    async list(request:express.Request,response:express.Response){
-        try{
-            const list=listInstrumentUseCase.execute();
+    async list(request: express.Request, response: express.Response) {
+        try {
+            const list = listInstrumentUseCase.execute();
             response.status(200).send(list);
-        }catch(error){
+        } catch (error) {
             console.log(error);
             response.status(500).send(constants.CRUD.ERROR);
         }
     }
 
-    async listID(request:express.Request,response:express.Response){
-        try{
-            const instrument=listIDInstrumentUseCase.execute(Number(request.params.id));
+    async listID(request: express.Request, response: express.Response) {
+        try {
+            const instrument = listIDInstrumentUseCase.execute(Number(request.params.id));
             response.status(200).send(instrument);
-        }catch(error){
+        } catch (error) {
             console.log(error);
             response.status(500).send(constants.CRUD.ERROR);
         }
     }
 
-    async update(request:express.Request,response:express.Response){
-        try{
-            const updated=updateInstrumentUseCase.execute(request.body,Number(request.params.id))
+    async update(request: express.Request, response: express.Response) {
+        try {
+            const updated = updateInstrumentUseCase.execute(request.body, Number(request.params.id))
             response.status(200).send(updated)
-        }catch(error){
+        } catch (error) {
             console.log(error)
             response.status(500).send(constants.CRUD.ERROR)
         }
     }
 
-    async delete(request:express.Request,response:express.Response){
-        try{
-            const deleted=deleteInstrumentUseCase.execute(Number(request.params.id))
+    async delete(request: express.Request, response: express.Response) {
+        try {
+            const deleted = deleteInstrumentUseCase.execute(Number(request.params.id))
             response.status(204).send()
-        }catch(error){
+        } catch (error) {
             console.log(error)
             response.status(500).send(constants.CRUD.ERROR)
         }
